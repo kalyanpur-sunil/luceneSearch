4359c89970f42027523562a764d44f30d0d7257f
kalyanpursunil85 <kalyanpursunil85@gmail.com>
2014-05-01 05:50:39 -0500

    Work In Progress

---
+++ luceneSearch/src/org/sunil/lucene/search/SimpleFileIndexer.java
@@ -0,0 +1,21 @@
+package org.sunil.lucene.search;
+
+import java.io.File;
+
+public class SimpleFileIndexer {
+	static final String INDEX_DIR = "C:/Users/Sunil/Work/ApacheLucene/LuceneIndexes/";
+	static final String DATA_DIR = "C:/Users/Sunil/Work/ApacheLucene/SearchData/";
+	
+	public static void main(String[] args) {
+		File indexDir = new File(INDEX_DIR);
+		File dataDir = new File(DATA_DIR);
+		
+		SimpleFileIndexer simpleFileIndexer = new SimpleFileIndexer();
+	}
+	
+	private int index(File indexDir, File dataDir, String suffix)
+	        throws Exception {
+		
+		return 0;
+	}
+}
